38a2f9f061df28faeb993a80edf5c922cbf50f58
se-master <64152321+se-master@users.noreply.github.com>
2020-05-31 00:09:31 -0400

    Add files via upload

---
+++ Graphe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp5
+{
+    public class Graphe : IDeepCopyable<Graphe>
+    {
+        private List<Noeud> Noeuds { get; set; }
+        public Graphe()
+        {
+            Noeuds = new List<Noeud>();
+        }
+        private Graphe(Graphe graphe)
+        {
+            List<Noeud> liste = graphe.Noeuds;
+            int n = liste.Count;
+            Noeuds = new List<Noeud>(n);
+            for (int i = 0; i < n; i++)
+                Noeuds.Add(liste[i]);
+        }
+
+        public Graphe DeepCopy()
+        {
+            return new Graphe(this);
+        }
+        public void AjouterNoeud(Noeud noeudÀAjouter)
+        {
+
+            foreach (Noeud noeud in Noeuds)
+            {
+                if (noeudÀAjouter == noeud)
+                {
+                    return;
+                }
+                Noeuds.Add(noeudÀAjouter);
+            }
+
+        }
+
+        public int CalculerNbChemins()
+        {
+            //int n = 0;
+            //int chemins=0;
+            //chemins = Noeuds[n].NbAdjacents
+            return Noeuds.Count;
+        }
+        public Noeud[] CopierNoeuds()
+        {
+            int capacité = Noeuds.Count;
+            Noeud[] Tableau = new Noeud[capacité];
+            for (int n = 0; n < capacité; ++n)
+            {
+                Tableau[n] = Noeuds[n].HybridCopy();
+            }
+            return Tableau;
+        }
+    }
+}
---
+++ Noeud.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+
+namespace tp5
+{
+    public class Noeud : IHybridCopyable<Noeud>
+    {
+        public string Étiquette { get; private set; }
+        public Vector2f Position { get; private set; }
+        public int NbAdjacents { get; private set; }
+        private List<Noeud> Adjacents { get; set; }
+
+        // Constructeur paramètrique
+        public Noeud(string étiquette, Vector2f position, int nbAdjacents)
+        {
+            Étiquette = étiquette;
+            Position = position;
+            NbAdjacents = nbAdjacents;
+            Adjacents = new List<Noeud>(nbAdjacents);
+        }
+        public Noeud HybridCopy()
+        {
+            return new Noeud(this);
+        }
+        private Noeud(Noeud noeud)
+        {
+            Étiquette = noeud.Étiquette;
+            Position = noeud.Position;
+            NbAdjacents = noeud.NbAdjacents;
+            Adjacents = noeud.Adjacents;
+        }
+
+        public void AjouterAdjacent(Noeud adjacent)
+        {
+            this.NbAdjacents += 1;
+            adjacent.NbAdjacents += 1;
+            this.Adjacents.Add(adjacent);
+            adjacent.Adjacents.Add(this);
+        }
+        public float CalculerDistanceAdjacents(int pos)
+        {
+            if (pos < 0){throw new ArgumentOutOfRangeException();}
+            float x1 = this.Position.X;
+            float x2 = Adjacents[pos].Position.X;
+            float y1 = this.Position.Y;
+            float y2 = Adjacents[pos].Position.Y;
+            float x = (x2 - x1);
+            float y = (y2 - y1);
+            return (float) Math.Sqrt(x*x + y*y);
+        }
+        public Noeud[] CopierAdjacents()
+        {
+            int capacité = this.Adjacents.Count;
+            Noeud[] tableau = new Noeud[capacité];
+            for (int n = 0; n < capacité; ++n)
+                tableau[n] = this.Adjacents[n].HybridCopy();
+            return tableau;
+        }
+        public string GénérerTexte()
+        {
+            int capacité = this.Adjacents.Count;
+            StringBuilder sb = new StringBuilder(this.Adjacents.Count);
+            string m;
+            for (int n = 0; n < capacité; ++n)
+            {
+                m +=this.Adjacents[n].Étiquette;
+            }
+
+            sb.ToString();
+
+            return $"{Étiquette} @ ({Position.X}, {Position.Y})\n--Adjacents--\n sb.ToString() \n";
+        }
+
+
+    }
+}
